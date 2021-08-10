b9acea7ce521fb25ead5babed1d168059d426a2c
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-08-10 22:15:56 +0500

    Added Express Public

--- Internship-D2/Expressjs/main.js
+++ Internship-D2/Expressjs/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {res.send('Hello World! from iceflame');})
+app.get(express.static(__dirname + '/public')); 
+app.get('/', (req, res) => {res.sendFile(__dirname + '/public/index.html');})
 app.get('/users',(req,res) =>{res.send('ALL USERS');})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
