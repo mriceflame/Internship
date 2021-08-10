a06a43494af47c0dd0bc17f172bb748e9b0ae0be
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-08-10 14:51:57 +0500

    Express JS,Express Engine,Routing

---
+++ Internship-D2/Expressjs/main.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {res.send('Hello World! from iceflame');})
+app.get('/users',(req,res) =>{res.send('ALL USERS');})
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
