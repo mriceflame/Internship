c4e9cc601261c90d9bdefcdb7b3d215d51e668da
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-08-11 17:39:39 +0500

    ADDED ROUTES

--- Internship-D2/Expressjs/main.js
+++ Internship-D2/Expressjs/main.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 app.get(express.static(__dirname + '/public')); 
-app.get('/', (req, res) => {res.sendFile(__dirname + '/public/index.html');})
-app.get('/users',(req,res) =>{res.send('ALL USERS');})
+app.get('/', (req, res) => {res.sendFile(__dirname + '/public/index.html');});
+// app.get('/user/:userID/profile',(req,res) =>{
+
+//     res.send('ALL USERS'+ req.params.userID);})
+
+app.get('/user/:userid?',(req,res) =>{
+
+    if(req.params.userid != undefined)
+    res.send('USERS '+ req.params.userid);
+    else 
+    res.sent('ALL USERS LIST');
+});
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
